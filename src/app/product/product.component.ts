de9e7bff43fe2455ec4a6f0dceca1f7c25230af9
ช Force <ForceWutthichai@gmail.com>
2024-07-13 22:39:10 +0700

    edit localStorage and url product error

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -36,34 +36,3 @@ export class ProductComponent implements OnInit {
     this.products = this._productService.getProducts();
   }
 }
-
-// import { Router } from '@angular/router';
-// import { Component, inject, NgModule, OnInit } from '@angular/core';
-// import { RouterModule } from '@angular/router';
-// import { NzButtonModule } from 'ng-zorro-antd/button';
-// import { ProductService } from '../service/product.service';
-// import { CommonModule } from '@angular/common';
-// @Component({
-//   selector: 'app-product',
-//   standalone: true,
-//   imports: [RouterModule,NzButtonModule,CommonModule],
-//   templateUrl: './product.component.html',
-//   styleUrl: './product.component.scss'
-// })
-// export class ProductComponent implements OnInit{
-//   readonly _productService = inject(ProductService);
-
-//   constructor(private route:Router){}
-//   ngOnInit(): void {
-//     // localStorage.clear();
-//   }
-//   welcomeHome(){
-//     this.route.navigate(['/welcome'])
-//   }
-//   onAdd(){
-//     this.route.navigate(['/addProduct'])
-//   }
-//   onDelete(productId: number) {
-//     this._productService.deleteProduct(productId);
-//   }
-// }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -30,9 +30,7 @@ export class ProductService {
   }
 
   private saveProducts() {
-    if (this.isLocalStorageAvailable()) {
-      localStorage.setItem('products', JSON.stringify(this.products));
-    }
+    localStorage.setItem('products', JSON.stringify(this.products));
   }
 
   private isLocalStorageAvailable(): boolean {
@@ -61,61 +59,3 @@ export class ProductService {
     this.saveProducts();
   }
 }
-
-// import { id_ID } from 'ng-zorro-antd/i18n';
-// import { products } from './../product';
-// import { Injectable } from "@angular/core"
-
-// export interface Product {
-//   id: number;
-//   name: string;
-//   details: string;
-//   price: number;
-//   stock: number;
-//   image: string;
-// }
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-
-// export class ProductService {
-//   products: Product[] = JSON.parse(localStorage.getItem('products') ?? '[]');
-
-
-//   ongetProduct() {
-//     this.products = JSON.parse(localStorage.getItem('products') ?? '[]')
-//   }
-//   // onsetProduct(){
-
-//     // }
-//   onAddProduct(
-//     id: number,
-//     productsName: string,
-//     productsDetails: string,
-//     productsPrice: number,
-//     productsStock: number,
-//     productsImage: string) {
-//     this.products.push({
-//       id: id,
-//       name: productsName,
-//       details: productsDetails,
-//       price: productsPrice,
-//       stock: productsStock,
-//       image: productsImage
-//     });
-//     console.log(this.products);
-
-//     localStorage.setItem('products', JSON.stringify(this.products));
-//     console.log(localStorage.getItem('products'));
-
-//     this.ongetProduct();
-
-//   }
-//   // onResetData(){
-
-//   // }
-//   deleteProduct(productId: number) {
-//     this.products = this.products.filter(product => product.id !== productId);
-//   }
-// }
